1d2d4451d0a870128e68006962dc890513582ce4
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-11-27 16:55:49 -0500

    Stuff

---
+++ CaptureGame/Assets/CompassScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompassScreen : MonoBehaviour
+{
+    [SerializeField]
+    Transform compassBearings;
+
+    public float bearing;
+
+
+    public void Update()
+    {
+        compassBearings.localEulerAngles = new Vector3(0, 0, bearing);
+        for(int i1 =0; i1 < compassBearings.childCount;i1++)
+        {
+            compassBearings.GetChild(i1).transform.eulerAngles = Vector3.zero;
+        }
+    }
+
+}
