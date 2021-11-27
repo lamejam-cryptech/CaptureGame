42aeca50f3a0889df7a0fe644f866af8c4d30c5b
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-11-27 17:21:40 -0500

    Added compass placeholder

--- CaptureGame/Assets/CompassScreen.cs
+++ CaptureGame/Assets/CompassScreen.cs
@@ -13,7 +13,7 @@ public class CompassScreen : MonoBehaviour
     public void Update()
     {
         compassBearings.localEulerAngles = new Vector3(0, 0, bearing);
-        for(int i1 =0; i1 < compassBearings.childCount;i1++)
+        for(int i1 = 0; i1 < compassBearings.childCount;i1++)
         {
             compassBearings.GetChild(i1).transform.eulerAngles = Vector3.zero;
         }
